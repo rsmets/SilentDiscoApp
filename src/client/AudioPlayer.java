81e630f4a19f3d1158f0777cb0c643ece0f0b2b4
Ray Smets <rayjsmets@gmail.com>
2014-03-13 20:07:10 -0700

    Added class for AudioFormat uniformity

--- src/client/AudioPlayer.java
+++ src/client/AudioPlayer.java
@@ -3,13 +3,13 @@ package client;
 import java.util.concurrent.BlockingQueue;
 
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioFormat.Encoding;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 import server.AudioGrabber;
+import shared.AudioFormatContainer;
 import shared.ByteArrayContainer;
 
 public class AudioPlayer implements Runnable{
@@ -20,11 +20,7 @@ public class AudioPlayer implements Runnable{
 	final BlockingQueue<ByteArrayContainer> audioQ;
 	int bufferSize;
 	
-	//globalish variables
-	public static final int sampleSizeInBits = 16;
-	public static final float sampleRate = 44100;
-	public static final float sampleFrameRate = 44100;
-	private static AudioFormat.Encoding ULAW;
+	//private static AudioFormat.Encoding ULAW;
 	
 	public AudioPlayer(BlockingQueue<ByteArrayContainer> queue) {
 		this.setup();
@@ -33,8 +29,9 @@ public class AudioPlayer implements Runnable{
 	
 	private void setup(){
 		//make format depending on input audio type
-	    format = new AudioFormat(sampleRate, sampleSizeInBits, 1, true, true);   
-	    //format = new AudioFormat(ULAW, sampleRate, sampleSizeInBits, 1, 1, sampleFrameRate, true);
+		format = new AudioFormatContainer().getAudioFormat();
+		
+	    //format = new AudioFormat(ULAW, sampleRate, sampleSizeInBits, 1, 1, sampleRate, true);
 	    outInfo = new DataLine.Info(SourceDataLine.class, format);
 	    bufferSize = (int) format.getSampleRate() * format.getFrameSize();
 	    //bufferSize = bufferSize / 16; //8000*2/16 = 1000
